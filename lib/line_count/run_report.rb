e521a6ce5b094c66410387d91eea510a48b554a0
Alistair McKinnell <alistair.mckinnell@gmail.com>
2015-03-11 14:39:15 -0400

    Adds the LineCount class; Adds the report task.

---
+++ lib/line_count/run_report.rb
@@ -0,0 +1,54 @@
+module LineCount
+  class RunReport
+
+    def self.generate(run_id)
+      new(run_id).generate
+    end
+
+    def initialize(run_id)
+      @run_id = run_id
+    end
+
+    def generate
+      puts format(results)
+    end
+
+    private
+
+    def format(results)
+      CSV.generate do |csv|
+        csv << headers
+
+        results.each do |result|
+          csv << result
+        end
+      end
+    end
+
+    def headers
+      ['Directory', 'Files', 'Lines']
+    end
+
+    def results
+      ActiveRecord::Base.connection.execute(sql).map { |row| row.values }
+    end
+
+    def sql
+      <<-SQL.strip_heredoc
+        SELECT
+          directory,
+          count(lines) as files,
+          sum(lines) as lines
+        FROM
+          code_counts
+        WHERE
+          run_id = #{@run_id}
+        GROUP BY
+          directory
+        ORDER BY
+          directory
+      SQL
+    end
+
+  end
+end
--- lib/tasks/sloc.rake
+++ lib/tasks/sloc.rake
@@ -1,5 +1,6 @@
 require_relative '../line_count/generator'
 require_relative '../line_count/importer'
+require_relative '../line_count/run_report'
 
 namespace :sloc do
 
@@ -25,6 +26,11 @@ namespace :sloc do
   desc 'List all runs of PackManager source code metrics'
   task :list => :environment do
     Run.all.each { |run| puts run.to_s }
+  end
+
+  desc 'Generate a report of PackManager source code metrics for the specified run'
+  task :report, [:run_id] => :environment do |_, args|
+    LineCount::RunReport.generate(args.run_id)
   end
 
 end
